8a5a30052bec4f72e939a6187b0fe9c5e52f7b5d
Tracy <tracy.atieno@student.moringaschool.com>
2022-11-24 00:51:24 +0300

    Array-Lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+ function destructivelyPrependCat(name){
+     cats.unshift(name)
+ }
+ function destructivelyRemoveLastCat(){
+    cats.pop()
+ }
+ function destructivelyRemoveFirstCat (){
+    cats.shift()
+ }
+ function appendCat(name){
+    let fit = [...cats]
+    fit.push(name)
+    return fit
+}
+function prependCat(name){
+    let law = [...cats]
+    law.unshift(name)
+    return law
+}
+function removeLastCat(){
+     let remove = [...cats]
+     remove.pop()
+     return remove
+}   
+function removeFirstCat(){
+    let can = [...cats]
+    can.shift()
+    return can
+}
+
